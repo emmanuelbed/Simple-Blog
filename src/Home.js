a167f28382623a584c9fd35e38941b71ed23d2e1
emmanuelbed <bedzoemmanuel8@gmail.com>
2023-10-20 05:23:21 -0500

    Adding a helper to log the error message onto the screen

--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,16 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
+  //Main Blogs State
   const [blogs, setBlogs] = useState(null);
+  //State to check the status of the data being requested
   const [isPending, setIsPending] = useState(true);
+  //State to return the error message
+  const [error, setError] = useState(null);
 
-  // const handleDelete = (id) => {
-  //   const newBlogs = blogs.filter((blog) => blog.id !== id);
-  //   setBlogs(newBlogs);
-  // };
   useEffect(() => {
     setTimeout(() => {
-      fetch("http://localhost:8000/blogss")
+      fetch("http://localhost:8000/blogs")
         .then((res) => {
           console.log(res);
           if (!res.ok) {
@@ -24,15 +24,18 @@ const Home = () => {
           // console.log(data);
           setBlogs(data);
           setIsPending(false);
+          setError(null);
         })
         .catch((err) => {
-          console.log(err.message);
+          setError(err.message);
+          setIsPending(false);
         });
     }, 1000);
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
     </div>
