fb169ab421a3e716d24d2013ff6aecff11af1f64
emmanuelbed <bedzoemmanuel8@gmail.com>
2023-10-20 04:51:10 -0500

    Connecting the JSON Api to the application

--- src/Home.js
+++ src/Home.js
@@ -2,16 +2,7 @@ import { useEffect, useState } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlogs] = useState([
-    { title: "My new Website", body: "Lorem Ipsum...", author: "mario", id: 1 },
-    { title: "Welcome Party!", body: "Lorem Ipsum...", author: "yoshi", id: 2 },
-    {
-      title: "Web dev top tips",
-      body: "Lorem Ipsum...",
-      author: "mario",
-      id: 3
-    }
-  ]);
+  const [blogs, setBlogs] = useState(null);
   const [name, setName] = useState("mario");
 
   const handleDelete = (id) => {
@@ -19,13 +10,25 @@ const Home = () => {
     setBlogs(newBlogs);
   };
   useEffect(() => {
-    console.log("use effect run");
-    console.log(name);
-  }, [name]);
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        // console.log(data);
+        setBlogs(data);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+      {blogs && (
+        <BlogList
+          blogs={blogs}
+          title="All Blogs!"
+          handleDelete={handleDelete}
+        />
+      )}
       <button onClick={() => setName("luigi")}>change name</button>
       <p>{name}</p>
     </div>
