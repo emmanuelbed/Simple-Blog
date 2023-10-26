208be4dd37455149d3bc9e24c24223080c0b7b87
emmanuelbed <bedzoemmanuel8@gmail.com>
2023-10-26 05:25:57 -0500

    Adding the redirect functionality after a new blog has been submitted

--- src/Create.js
+++ src/Create.js
@@ -1,10 +1,12 @@
 import { useState } from "react";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
 const Create = () => {
   const [title, setTitle] = useState("");
   const [body, setBody] = useState("");
   const [author, setAuthor] = useState("mario");
   const [isPending, setIsPending] = useState(false);
+  const history = useHistory();
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -20,6 +22,9 @@ const Create = () => {
       console.log("New Blog Added");
       setIsPending(false);
     });
+
+    // history.go(-1);
+    history.push("/");
   };
 
   return (
